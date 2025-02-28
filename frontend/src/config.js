e88dcfe0a3e09eb2bd477d8817fa24234f24695f
Berke <berke94gunen@gmail.com>
2025-02-28 19:36:03 +0300

    Updated API URL for Heroku deployment

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
 
 export const API_ENDPOINTS = {
     CUSTOMERS: `${API_BASE_URL}/customers`,
