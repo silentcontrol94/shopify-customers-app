4dae67bb9bb3ce24e78fcf6d9c4217139ff669fb
Berke <berke94gunen@gmail.com>
2025-02-28 19:52:11 +0300

    test env

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,8 @@
 const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
 
 console.log("API_BASE_URL:", API_BASE_URL);
+console.log("SHOPIFY_STORE_URL:", process.env.SHOPIFY_STORE_URL);
+console.log("SHOPIFY_ACCESS_TOKEN:",process.env.SHOPIFY_ACCESS_TOKEN);
 
 export const API_ENDPOINTS = {
     CUSTOMERS: `${API_BASE_URL}/customers`,
